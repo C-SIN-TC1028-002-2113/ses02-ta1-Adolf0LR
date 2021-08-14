32c74a235df206a347c6eb44367eddc44355a732
Adolf0LR <A01740785@itesm.mx>
2021-08-14 19:02:19 +0000

    Ejercicio 5

--- assignments/05Lustros/src/exercise.py
+++ assignments/05Lustros/src/exercise.py
@@ -1,7 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
+    nacer = float(input("Dame el año de nacimiento: "))
+    año = float(input("Dame el año actual: "))
+    edad = año - nacer
+    lustros = edad/5
+    print("Los lustros que has vivido son:",lustros)
 
 
 if __name__ == '__main__':
