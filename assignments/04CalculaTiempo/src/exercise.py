c35779b4741f82698ae76cb5924376af22107cf6
Adolf0LR <A01740785@itesm.mx>
2021-08-14 18:56:27 +0000

    Ejercicio 4

--- assignments/04CalculaTiempo/src/exercise.py
+++ assignments/04CalculaTiempo/src/exercise.py
@@ -1,9 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
-
-
+    age = int(input("Dame tu edad: "))
+    year = int(input("Dame el año actual: "))
+    nacimiento = year-age
+    futuro = nacimiento + 100
+    print("Cumplirás 100 años en el año:",futuro)
 
 if __name__ == '__main__':
     main()
